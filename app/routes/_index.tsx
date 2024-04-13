a2805a3721eccc1484b0af48b0a8c311e5f05726
Explosion 8or <ebartek@nd.edu>
2024-04-13 15:51:21 -0400

    server and how to use in index

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,7 +1,10 @@
-import type { MetaFunction, LinksFunction } from "@remix-run/node";
+import type { MetaFunction, LinksFunction, LoaderFunction } from "@remix-run/node";
+import { json } from "@remix-run/node";
 import {
-  Outlet,
+  useLoaderData,
 } from "@remix-run/react";
+
+import {getActivites} from "~/data.ts"
 
 import styles from "../styles/styles.css";
 
@@ -10,6 +13,12 @@ export const links: LinksFunction = () => [
   { rel: "stylesheet", href: styles },
 ];
 
+export async function loader() {
+  const acts:Activity[] = await getActivites();
+  
+  return json(acts);
+}
+
 export const meta: MetaFunction = () => {
   return [
     { title: "New Remix App" },
@@ -17,35 +26,15 @@ export const meta: MetaFunction = () => {
   ];
 };
 
+
 export default function Index() {
+  const things = useLoaderData();
   return (
     <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8"}}>
-      <h1>Welcome to Remix</h1>
-      <ul>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/blog"
-            rel="noreferrer"
-          >
-            15m Quickstart Blog Tutorial gamer
-          </a>
-        </li>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/jokes"
-            rel="noreferrer"
-          >
-            Deep Dive Jokes App Tutorial
-          </a>
-        </li>
-        <li>
-          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-            Remix Docs
-          </a>
-        </li>
-      </ul>
+      <p> heyo </p>
+      {things.map((activity) => (
+        <div key={activity.name}>{activity.name}</div>
+      ))}
     </div>
   );
 }
