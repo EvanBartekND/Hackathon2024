ed36830dfe4d1ab31a1230478e4b7f23ebfee488
Evan <ebartek@nd.edu>
2024-04-13 11:45:48 -0400

    landing page ex

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,4 +1,8 @@
 import type { MetaFunction } from "@remix-run/node";
+import {
+  Outlet,
+} from "@remix-run/react";
+import Landing from "./landing.tsx"
 
 export const meta: MetaFunction = () => {
   return [
@@ -9,7 +13,7 @@ export const meta: MetaFunction = () => {
 
 export default function Index() {
   return (
-    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
+    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" , background:"red"}}>
       <h1>Welcome to Remix</h1>
       <ul>
         <li>
@@ -36,6 +40,7 @@ export default function Index() {
           </a>
         </li>
       </ul>
+      <Landing />
     </div>
   );
 }
---
+++ app/routes/landing.tsx
@@ -0,0 +1,14 @@
+
+
+export default function Landing() {
+    return (
+    <div >
+        <img alt="a logo"/>
+        <br/>
+        <button> hi </button>
+        <br />
+        <p> Meeting New People </p>
+        <p> Doing New Things </p>
+    </div>
+ );
+}
